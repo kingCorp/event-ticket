265cff894f0c8bc96449f279650651fa0de3466d
ifeanyi <christify30@gmail.com>
2020-12-02 04:45:54 +0100

    edit delete ticket

--- src/pages/eventDetails/eventDetails.jsx
+++ src/pages/eventDetails/eventDetails.jsx
@@ -21,6 +21,9 @@ import { useHistory } from "react-router-dom";
 import QRCode from "qrcode.react";
 import dayjs from "dayjs";
 
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
 const EventsDetails = (props) => {
   const {
     match: { params },
@@ -37,20 +40,46 @@ const EventsDetails = (props) => {
     quantity: "",
     succ: false,
   });
+  const [formEdit, setFormEdit] = useState({
+    code: "",
+    phone: "",
+    price: "",
+    quantity: "",
+    succ: false,
+  });
 
   const [open, setOpen] = React.useState(false);
+  const [openEdit, setOpenEdit] = React.useState(false);
 
   const columns = [
-    { title: "Ticket code", field: "code" },
-    { title: "Phone", field: "phone" },
-    { title: "Price", field: "price", type: "numeric" },
-    { title: "Quantity", field: "quantity", type: "numeric" },
-    { title: "Status", field: "status", render: ticket =>
-    <span className={ticket.status==="pending" ? "badge badge-warning" : "badge badge-success"}>{ticket.status}</span> },
-    { title: "Corkage", field: "corkage", render: ticket =>
-    <span className={ticket.corkage==="pending" ? "badge badge-warning" : "badge badge-success"}>{ticket.corkage}</span> },
-    { title: "Date", field: "date", render: ticket =>
-    dayjs(ticket.date).format('YYYY-MM-DD') },
+    {
+      title: "Ticket code", field: "code", render: ticket =>
+        <small>{ticket.code}</small>
+    },
+    {
+      title: "Phone", field: "phone", render: ticket =>
+        <small>{ticket.phone}</small>
+    },
+    {
+      title: "Price", field: "price", type: "numeric", render: ticket =>
+        <small>{ticket.price}</small>
+    },
+    {
+      title: "Quantity", field: "quantity", type: "numeric", render: ticket =>
+        <small>{ticket.quantity}</small>
+    },
+    {
+      title: "Status", field: "status", render: ticket =>
+        <span className={ticket.status === "pending" ? "badge badge-warning" : "badge badge-success"}>{ticket.status}</span>
+    },
+    {
+      title: "Corkage", field: "corkage", render: ticket =>
+        <span className={ticket.corkage === "pending" ? "badge badge-warning" : "badge badge-success"}>{ticket.corkage}</span>
+    },
+    {
+      title: "Date", field: "date", render: ticket =>
+        <small>{dayjs(ticket.date).format('YYYY-MM-DD')}</small>
+    },
   ];
 
   const handleClickOpen = () => {
@@ -59,6 +88,10 @@ const EventsDetails = (props) => {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleCloseEdit = () => {
+    setOpenEdit(false);
   };
 
   const createTicket = async () => {
@@ -84,6 +117,46 @@ const EventsDetails = (props) => {
     }
   };
 
+  const editTicket = async () => {
+    const ticketdata = {
+      ...formEdit,
+     // code: formEdit.code.toUpperCase(),
+    };
+    try {
+      const res = await axios.put(
+        `${BASE_API}/ticket/${formEdit._id}`,
+        ticketdata
+      );
+      if (res.data.hasError) {
+        swal("Failed", res.data.message, "error");
+      } else {
+        swal("Success", res.data.message, "success");
+        setFormEdit({ ...form, succ: true });
+        refetch();
+        setOpenEdit(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteTicket = async (id) => {
+  
+    try {
+      const res = await axios.delete(
+        `${BASE_API}/ticket/${id}`
+      );
+      if (res.data.hasError) {
+        swal("Failed", res.data.message, "error");
+      } else {
+        swal("Deleted", "ticket deleted", "success");
+        refetch();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const downloadQR = () => {
     // will implement soon
     const canvas = document.getElementById(form.code.toUpperCase());
@@ -97,6 +170,35 @@ const EventsDetails = (props) => {
     downloadLink.click();
     document.body.removeChild(downloadLink);
   };
+
+  const onEditDialog = (ticket) => {
+    setOpenEdit(true);
+    setFormEdit({...formEdit,
+    //code: ticket.code,
+    phone: ticket.phone,
+    _id: ticket._id,
+    price: ticket.price,
+    quantity: ticket.quantity
+    })
+  }
+
+  const handleDelete = (id) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        deleteTicket(id);
+      } else {
+        // swal("Your imaginary file is safe!");
+      }
+    });
+  };
+
+
 
   if (status === "laoding") {
     return <Spinner />;
@@ -159,8 +261,35 @@ const EventsDetails = (props) => {
           )}
 
           <div className="ticket-info">
-            <div style={{ maxWidth: "100%", marginTop: 20, padding:10 }}>
-              <MaterialTable columns={columns} data={data} title="Tickets"  />
+            <div style={{ maxWidth: "100%", marginTop: 20, padding: 10 }}>
+              <MaterialTable columns={columns} data={data} title="Tickets"
+
+                actions={[
+                  () => ({
+                    // eslint-disable-next-line react/display-name
+                    icon: () => <EditIcon />,
+                    tooltip: 'Edit Ticket',
+                    onClick: (
+                      e,
+                      data
+                    ) => onEditDialog(data),
+                  }),
+                  () => ({
+                    // eslint-disable-next-line react/display-name
+                    icon: () => <DeleteIcon color='error' />,
+                    tooltip: 'Delete Ticket',
+                    onClick: (
+                      e,
+                      data
+                    ) => handleDelete(data._id),
+                  })
+                ]}
+
+                options={{
+                  actionsColumnIndex: -1
+                }}
+
+              />
             </div>
           </div>
         </div>
@@ -170,7 +299,7 @@ const EventsDetails = (props) => {
           onClose={handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">Create Event</DialogTitle>
+          <DialogTitle id="form-dialog-title">Create Ticket</DialogTitle>
           <DialogContent>
             <TextField
               autoFocus
@@ -205,6 +334,14 @@ const EventsDetails = (props) => {
               fullWidth
               onChange={(e) => setForm({ ...form, quantity: e.target.value })}
             />
+            <TextField
+              margin="dense"
+              id="code"
+              label="Passcode"
+              type="password"
+              fullWidth
+              onChange={(e) => setForm({ ...form, passcode: e.target.value })}
+            />
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose} color="primary">
@@ -212,6 +349,70 @@ const EventsDetails = (props) => {
             </Button>
             <Button onClick={createTicket} color="primary">
               Create
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        <Dialog
+          open={openEdit}
+          onClose={handleCloseEdit}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Edit Ticket</DialogTitle>
+          <DialogContent>
+            {/* <TextField
+              autoFocus
+              margin="dense"
+              id="code"
+              label="Ticket code"
+              type="text"
+              fullWidth
+              value={formEdit.code}
+              onChange={(e) => setFormEdit({ ...formEdit, code: e.target.value })}
+            /> */}
+            <TextField
+              margin="dense"
+              id="phone"
+              label="Customer Phone number"
+              type="text"
+              fullWidth
+              value={formEdit.phone}
+              onChange={(e) => setFormEdit({ ...formEdit, phone: e.target.value })}
+            />
+            <TextField
+              margin="dense"
+              id="price"
+              label="Price"
+              type="number"
+              fullWidth
+              value={formEdit.price}
+              onChange={(e) => setFormEdit({ ...formEdit, price: e.target.value })}
+            />
+            <TextField
+              margin="dense"
+              id="quantity"
+              label="Quantity"
+              type="number"
+              fullWidth
+              value={formEdit.quantity}
+              onChange={(e) => setFormEdit({ ...formEdit, quantity: e.target.value })}
+            />
+            <TextField
+              
+              margin="dense"
+              id="code"
+              label="Passcode"
+              type="password"
+              fullWidth
+              onChange={(e) => setFormEdit({ ...formEdit, passcode: e.target.value })}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseEdit} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={editTicket} color="primary">
+              Edit
             </Button>
           </DialogActions>
         </Dialog>
